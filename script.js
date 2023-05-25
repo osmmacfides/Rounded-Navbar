abf7a237279628458507ed2de49b667a88f8a467
osmmacfides <okonkwosmmac@gmail.com>
2023-05-25 19:16:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbarBTN = document.querySelector(".navbar-btn");
+const navbarWrapper = document.querySelector(".navbar-wrapper");
+
+
+navbarBTN.addEventListener("click", () => {
+  navbarWrapper.classList.toggle("change");
+ })
